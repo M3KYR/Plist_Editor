82873d414ff375b670038f4bbc70fb745d81889f
M3KYR <pjgomez_1997@tutanota.com>
2020-06-26 14:16:48 +0200

    Fixes and cleanup

--- src/com/m3kyr/plist/PList_Editor.java
+++ src/com/m3kyr/plist/PList_Editor.java
@@ -12,15 +12,14 @@ import com.dd.plist.*;
 import java.io.*;
 
 public class PList_Editor {
-
-    JFrame frame;
-    JPanel mainPanel;
-    JTree plistTree;
-    DefaultTreeCellRenderer cellRenderer;
-    JTextField textField = new JTextField();
-    TreeCellEditor editor = new DefaultCellEditor(textField);
-    NSDictionary rootDict;
-    DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
+    private JFrame frame;
+    private JPanel mainPanel;
+    private JTree plistTree;
+    private DefaultTreeCellRenderer cellRenderer;
+    private JTextField textField = new JTextField();
+    private TreeCellEditor editor = new DefaultCellEditor(textField);
+    private NSDictionary rootDict;
+    private DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
 
     public static void main(String[] args) {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
@@ -29,66 +28,61 @@ public class PList_Editor {
     }
 
     private void go() {
+        editor.addCellEditorListener(new cellEditListener());
+        JMenuItem openMenuItem = setupOpenMenuItem();
+        JMenuItem saveMenuItem = setupSaveMenuItem();
+        JMenuItem exitMenuItem = setupExitMenuItem();
+        JMenu fileMenu = setupFileMenu(openMenuItem, saveMenuItem, exitMenuItem);
+        JMenuBar menuBar = setupMenuBar(fileMenu);
+        setupMainPanel();
+        setupFrame(menuBar);
+    }
+
+    private void setupFrame(JMenuBar menuBar) {
         frame = new JFrame("PList Editor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainPanel = new JPanel();
-        mainPanel.setSize(500, 500);
-
-        editor.addCellEditorListener(new cellEditListener());
-
-        JMenuBar menuBar = new JMenuBar();
-
-        JMenu fileMenu = new JMenu("File");
-
-        JMenuItem openFile = new JMenuItem("Open");
-        openFile.addActionListener(new openFileListener());
-
-        JMenuItem saveFile = new JMenuItem("Save");
-        saveFile.addActionListener(new saveFileListener());
-        
-        JMenuItem exit = new JMenuItem("Exit");
-        exit.addActionListener(new exitListener());
-
-        fileMenu.add(openFile);
-        fileMenu.add(saveFile);
-        fileMenu.add(exit);
-
-        menuBar.add(fileMenu);
-
         frame.getContentPane().add(mainPanel);
-
         frame.setJMenuBar(menuBar);
         frame.setSize(500, 500);
         frame.setVisible(true);
+    }
+
+    private void setupMainPanel() {
+        mainPanel = new JPanel();
+        mainPanel.setSize(500, 500);
+    }
+
+    private JMenuBar setupMenuBar(JMenu fileMenu) {
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(fileMenu);
+        return menuBar;
+    }
+
+    private JMenu setupFileMenu(JMenuItem openFile, JMenuItem saveFile, JMenuItem exit) {
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.add(openFile);
+        fileMenu.add(saveFile);
+        fileMenu.add(exit);
+        return fileMenu;
+    }
+
+    private JMenuItem setupOpenMenuItem() {
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        openMenuItem.addActionListener(new openFileListener());
+        return openMenuItem;
     }
 
     private class openFileListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             JFileChooser fileOpen = new JFileChooser();
             fileOpen.showOpenDialog(frame);
-            setupPlist(fileOpen.getSelectedFile());
-        }
-    }
-
-    private class saveFileListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            JFileChooser fileSave = new JFileChooser();
-            fileSave.setAcceptAllFileFilterUsed(false);
-            fileSave.setFileFilter(new FileNameExtensionFilter("Property List", "plist"));
-            fileSave.showSaveDialog(frame);
-            if(fileSave.getSelectedFile() != null) {
-                savePlist(fileSave.getSelectedFile());
+            if (fileOpen.getSelectedFile() != null) {
+                setupPlist(fileOpen.getSelectedFile());
             }
         }
     }
 
-    private class exitListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.exit(0);
-        }
-    }
-
-    void setupPlist(File f) {
+    private void setupPlist(File f) {
         try {
             rootDict = (NSDictionary) PropertyListParser.parse(f);
             for (String key : rootDict.allKeys()) {
@@ -98,13 +92,8 @@ public class PList_Editor {
                     addPlistNode(node,rootDict.objectForKey(key));
                 }
             }
-            plistTree = new JTree(root);
-            plistTree.setEditable(true);
-            plistTree.setCellEditor(editor);
-            cellRenderer = (DefaultTreeCellRenderer) plistTree.getCellRenderer();
-            cellRenderer.setLeafIcon(null);
-            cellRenderer.setClosedIcon(null);
-            cellRenderer.setOpenIcon(null);
+            setupPlistJTree();
+            setupCellRenderer();
             frame.getContentPane().add(new JScrollPane(plistTree));
             frame.validate();
         } catch (Exception e) {
@@ -130,12 +119,55 @@ public class PList_Editor {
         }
     }
 
+    private void setupPlistJTree() {
+        plistTree = new JTree(root);
+        plistTree.setEditable(true);
+        plistTree.setCellEditor(editor);
+    }
+
+    private void setupCellRenderer() {
+        cellRenderer = (DefaultTreeCellRenderer) plistTree.getCellRenderer();
+        cellRenderer.setLeafIcon(null);
+        cellRenderer.setClosedIcon(null);
+        cellRenderer.setOpenIcon(null);
+    }
+
+    private JMenuItem setupSaveMenuItem() {
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.addActionListener(new saveFileListener());
+        return saveMenuItem;
+    }
+
+    private class saveFileListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            JFileChooser fileSave = new JFileChooser();
+            fileSave.setAcceptAllFileFilterUsed(false);
+            fileSave.setFileFilter(new FileNameExtensionFilter("Property List", "plist"));
+            fileSave.showSaveDialog(frame);
+            if (fileSave.getSelectedFile() != null) {
+                savePlist(fileSave.getSelectedFile());
+            }
+        }
+    }
+
     private void savePlist (File f) {
         try {
             PropertyListParser.saveAsXML(rootDict,f);
         }
         catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private JMenuItem setupExitMenuItem() {
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(new exitListener());
+        return exitMenuItem;
+    }
+
+    private class exitListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.exit(0);
         }
     }
 
@@ -148,4 +180,5 @@ public class PList_Editor {
             Object value = editor.getCellEditorValue();
         }
     }
+
 }
